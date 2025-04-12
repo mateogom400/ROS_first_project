122d37a4bf9d3e50eca87942c6c1213b28f2890a
Your Mateo_Gomez <mateogomezab18@hotmail.com>
2025-04-12 18:51:19 +0200

    publisher first try

---
+++ catkin_ws/src/first_project/src/gps_odometer_pub_sub.cpp
@@ -0,0 +1,33 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+
+
+int main(int argc, char **argv){
+
+	ros::init(argc, argv, "talker"); //, ros::init_options::AnonymousName);
+	ros::NodeHandle n;
+
+	ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter", 1);
+
+	ros::Rate loop_rate(10);
+
+	while (ros::ok()){
+
+	    	std_msgs::String msg;
+
+                msg.data = "hello world!";
+
+    		ROS_INFO("%s", msg.data.c_str());
+
+    		chatter_pub.publish(msg);
+
+    		ros::spinOnce();
+
+    		loop_rate.sleep();
+    	
+  	}
+
+
+  	return 0;
+}
